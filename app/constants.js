a4066ca0d66a75521bbe917fd609df85c92f0262
abdulsamedkeskin <erdemsametkeskin25@gmail.com>
2023-08-04 00:17:19 +0300

    updated the redirect uris

--- app/constants.js
+++ app/constants.js
@@ -2,11 +2,13 @@ export const spotifyBaseUrl = "https://accounts.spotify.com/authorize";
 export const spotifyClientId = "0ed70de2a1a2441e949814684bd4c609";
 export const spotifyScope =
   "playlist-modify-public playlist-read-private playlist-read-collaborative playlist-modify-private";
-export const spotifyRedirectUri = "http://localhost:3000/callback";
+export const spotifyRedirectUri =
+  "https://playlist-converter-three.vercel.app/callback";
 
 export const youtubeBaseUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 export const youtubeScope =
   "https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube";
 export const youtubeClientId =
   "1018098713193-jbbigdpefo1961tjor7g87t4bku2bk1e.apps.googleusercontent.com";
-export const youtubeRedirectUri = "http://localhost:3000/callback";
+export const youtubeRedirectUri =
+  "https://playlist-converter-three.vercel.app/callback";
